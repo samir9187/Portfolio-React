45645d8c10550b5814e60169ed47ce3f20a81815
samir 9187 <samirkunwar9187@gmail.com>
2024-08-06 19:28:09 +0530

    footer problem solve

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/samir9187"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
